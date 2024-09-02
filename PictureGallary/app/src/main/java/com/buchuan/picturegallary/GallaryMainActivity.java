905fa838300c5f4c35d207485ca5899757aedd20
Shenjie(沈杰) <745689122@qq.com>
2024-09-02 08:51:08 +0800

    实现底部tabbar及fragment页面切换
    实现底部tabbar及fragment页面切换

---
+++ PictureGallary/app/src/main/java/com/buchuan/picturegallary/GallaryMainActivity.java
@@ -0,0 +1,42 @@
+package com.buchuan.picturegallary;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.buchuan.picturegallary.databinding.ActivityGallaryMainBinding;
+
+public class GallaryMainActivity extends AppCompatActivity {
+
+    private ActivityGallaryMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityGallaryMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // 查找并设置Toolbar为ActionBar
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_gallary_main);
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+}
