0117712470f1237162434d2b64a4b6bb5cf84ec0
Shenjie(沈杰) <745689122@qq.com>
2024-09-04 08:53:20 +0800

    recycleview的使用，code 还未测试通过，只是先提交

---
+++ PictureGallary/app/src/main/java/com/buchuan/picturegallary/ui/home/MyAdapter.java
@@ -0,0 +1,54 @@
+package com.buchuan.picturegallary.ui.home;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.buchuan.picturegallary.R;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    private List<Person> personList;
+
+    // Constructor for passing the data list
+    public MyAdapter(List<Person> personList) {
+        this.personList = personList;
+    }
+
+    // ViewHolder class for caching view references
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView textView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.textView);
+        }
+    }
+
+    // Create new views (invoked by the layout manager)
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_view, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Person person = personList.get(position);
+        holder.textView.setText(person.getName() + ", Age: " + person.getAge());
+    }
+
+    // Return the size of the dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return personList.size();
+    }
+}
